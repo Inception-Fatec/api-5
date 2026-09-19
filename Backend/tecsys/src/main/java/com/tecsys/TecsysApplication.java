95c97ce3d92015ce5fb375b9253db7e0fb9db5a4
GKanjiscuk <gi.kanjiscuk@gmail.com>
2026-09-19 12:25:21 -0300

    feat: configurar documentacao swagger com suporte a autenticacao jwt

--- Backend/tecsys/src/main/java/com/tecsys/TecsysApplication.java
+++ Backend/tecsys/src/main/java/com/tecsys/TecsysApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TecsysApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TecsysApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TecsysApplication.class, args);
+    }
 
 }
--- Backend/tecsys/src/main/java/com/tecsys/config/SecurityConfig.java
+++ Backend/tecsys/src/main/java/com/tecsys/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                        .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(customizer -> customizer
